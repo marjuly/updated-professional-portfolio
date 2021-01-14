994f5399b744228dbe50e67d7214d5bf793c2e23
marjuly <56303827+marjuly@users.noreply.github.com>
2021-01-14 09:59:00 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# updated-professional-portfolio
+# new-digital-portfolio
 Created with CodeSandbox
---
+++ js/script.js
@@ -0,0 +1,12 @@
+function scrollWin1() {
+  window.scrollTo(0, 0);
+}
+function scrollWin2() {
+  window.scrollTo(0, 740);
+}
+function scrollWin3() {
+  window.scrollTo(0, 1320);
+}
+function ScrollToBottom() {
+  window.scrollTo(0, document.body.scrollHeight);
+}
